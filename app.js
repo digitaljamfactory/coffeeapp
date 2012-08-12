320f5c7537d88a477a14f7fbebb4675fbffd9dac
Anthony <anthony@ubuntu.ubuntu-domain>
2012-08-12 22:10:33 +1000

    add customer and cafe login

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 var application_root = __dirname,
     express = require("express"),
     path = require("path"),
-    mongoose = require("mongoose");
-
+    mongoose = require("mongoose"),
+    salt = 'mySaltyString',
+    SHA2 = new (require('jshashes').SHA512),
+    tokenGenerator = new require('crypto');
 
 var app = express.createServer();
+
+var auth = express.basicAuth(function (username, password) {
+   return (username === process.env.auth_username && password === process.env.auth_password);
+}, "Failure");
+
+function encodePassword( password ){
+    if( typeof password === 'string' && password.length < 6 ) return ''
+    return SHA2.b64_hmac(password, salt )
+}
+
+function generateToken() {
+  return tokenGenerator.randomBytes(24).toString('hex');
+}
 
 
 //Database
@@ -25,18 +40,19 @@ var Schema = mongoose.Schema;
 // Schemas
 
 var Customer = new Schema({
-
    pickUpName: { type: String, required: true},
-   emailAddress: { type: String, required: true},
-   password: { type: String, required: true},
-   cafeName: { type: String, required: true}
+   emailAddress: { type: String, required: true, unique: true},
+   password: { type: String, set: encodePassword, required: true},
+   cafeName: { type: String, required: true},
+   token: { type: String }
 });
 
 var Cafe = new Schema({
-   name: { type: String, required: true},
-   emailAddress: { type: String, required: true},
-   password: { type: String, required: true},
-   address: { type: String, required: true}
+   name: { type: String, required: true, unique: true},
+   emailAddress: { type: String, required: true, unique: true},
+   password: { type: String, set: encodePassword, required: true},
+   address: { type: String, required: true},
+   token: {type: String}
 });
 
 var Sugars = new Schema({
@@ -57,6 +73,7 @@ var Coffees = new Schema({
 var Order = new Schema({  
     customerId: { type: String, required: true},
     pickUpName: { type: String, required: true },  
+    token: {type: String, required: true},
     cafe: { type: String, required: true },  
     pickUpTime: { type: Date, required: true },
     status: { type: String, required: true },
@@ -65,16 +82,137 @@ var Order = new Schema({
 });
    
 var OrderModel = mongoose.model('Order',Order);
+var CustomerModel = mongoose.model('Customer',Customer);
+var CafeModel = mongoose.model('Cafe', Cafe);
+
+app.post('/api/customers', function (req, res) {
+  var customer;
+  var cToken = generateToken();
+  customer = new CustomerModel({
+     pickUpName: req.body.pickUpName,
+     emailAddress: req.body.emailAddress,
+     password: req.body.password,
+     cafeName: req.body.cafeName,
+     token: cToken    
+  });
+  customer.save(function (err) {
+    if (!err) {
+      return res.send(cToken);
+    } else {
+      res.send(err);
+      return console.log(err);
+    }
+  });
+});
+
+app.get('/api/customers', auth, function (req, res){
+  return CustomerModel.find(function (err, customers) {
+    console.log(err);
+    if(!err) {
+       return res.send(customers);
+    } else {
+       return console.log(err);
+    }
+  });
+});
+
+app.get('/api/customers/login', auth, function (req, res){
+  var query = CustomerModel.findOne({});
+ 
+  query.where('emailAddress',req.query["login"]);
+  query.where('password', encodePassword(req.query["password"]));
+
+  query.exec( function (err, customer) {
+    if (!err) {     
+      if(customer) { 
+         return res.send(customer.token);
+      } else {
+         return res.send(403,"Invalid login or password");
+      }
+    } else {
+      return console.log(err);
+    }
+  });
+});
+
+app.delete('/api/customers', auth, function (req, res) {
+  CustomerModel.remove(function (err) {
+    if (!err) {
+      return res.send('');
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.post('/api/cafes', function (req, res) {
+  var cafe;
+  var cToken = generateToken();
+  cafe = new CafeModel({
+     name: req.body.name,
+     emailAddress: req.body.emailAddress,
+     password: req.body.password,
+     address: req.body.address,
+     token: cToken    
+  });
+  cafe.save(function (err) {
+    if (!err) {
+      return res.send(cToken);
+    } else {
+      res.send(err);
+      return console.log(err);
+    }
+  });
+});
+
+app.get('/api/cafes', auth, function (req, res){
+  return CafeModel.find({}, 'name', function (err, cafes) {
+    console.log(err);
+    if(!err) {
+       return res.send(cafes);
+    } else {
+       return console.log(err);
+    }
+  });
+});
+
+app.get('/api/cafes/login', auth, function (req, res){
+  var query = CafeModel.findOne({});
+ 
+  query.where('emailAddress',req.query["login"]);
+  query.where('password', encodePassword(req.query["password"]));
+
+  query.exec( function (err, cafe) {
+    if (!err) {     
+      if(cafe) { 
+         return res.send(cafe.token);
+      } else {
+         return res.send(403,"Invalid login or password");
+      }
+    } else {
+      return console.log(err);
+    }
+  });
+});
+
+app.delete('/api/cafes', auth, function (req, res) {
+  CafeModel.remove(function (err) {
+    if (!err) {
+      return res.send('');
+    } else {
+      console.log(err);
+    }
+  });
+});
 
 app.get('/api', function(req,res) {
   res.send('Coffee App API is running');
 });
 
-app.get('/api/orders', function (req, res){
+app.get('/api/orders', auth, function (req, res){
   return OrderModel.find(function (err, orders) {
     console.log(err);
     if(!err) {
-       console.log(orders);
        return res.send(orders);
     } else {
        return console.log(err);
@@ -82,33 +220,32 @@ app.get('/api/orders', function (req, res){
   });
 });
 
-app.post('/api/orders', function (req, res){
+
+
+app.post('/api/orders', auth, function (req, res){
   var order;
-  console.log("POST: ");
-  console.log(req.body);
   order = new OrderModel({
     customerId: req.body.customerId,
     pickUpName: req.body.pickUpName,
     cafe: req.body.cafe,
     pickUpTime: req.body.pickUpTime,
     status: req.body.status,
-    coffees: req.body.coffees    
+    coffees: req.body.coffees,
+    token: req.body.token   
   });
   order.save(function (err) {
     if (!err) {
-      return console.log("created");
+      return '';
     } else {
       return console.log(err);
     }
   });
-  return res.send(order);
+  return res.send(order.id);
 });
 
-app.get('/api/orders/:id', function (req, res){
-  console.log(req.params.id);
+app.get('/api/orders/:id', auth, function (req, res){
   return OrderModel.findById(req.params.id, function (err, order) {
     if (!err) {     
-      console.log(order);
       return res.send(order);
     } else {
       return console.log(err);
@@ -116,8 +253,7 @@ app.get('/api/orders/:id', function (req, res){
   });
 });
 
-app.get('/api/orders/cafe/:name', function (req, res){
-  console.log(req.params.name);
+app.get('/api/orders/cafe/:name/:token', auth, function (req, res){
   var query = OrderModel.find({});
  
   query.where('cafe',req.params.name);
@@ -125,7 +261,6 @@ app.get('/api/orders/cafe/:name', function (req, res){
 
   query.exec( function (err, orders) {
     if (!err) {     
-      console.log(orders);
       return res.send(orders);
     } else {
       return console.log(err);
@@ -135,7 +270,7 @@ app.get('/api/orders/cafe/:name', function (req, res){
 
 
 
-app.put('/api/orders/:id', function (req, res){
+app.put('/api/orders/:id/:token', auth, function (req, res){
   return OrderModel.findById(req.params.id, function (err, order) {
     order.status = req.body.status
     return order.save(function (err) {
@@ -149,11 +284,10 @@ app.put('/api/orders/:id', function (req, res){
   });
 });
 
-app.delete('/api/orders/:id', function (req, res){
+app.delete('/api/orders/:id', auth, function (req, res){
   return OrderModel.findById(req.params.id, function (err, order) {
     return order.remove(function (err) {
       if (!err) {
-        console.log("removed");
         return res.send('');
       } else {
         console.log(err);
@@ -162,10 +296,9 @@ app.delete('/api/orders/:id', function (req, res){
   });
 });
 
-app.delete('/api/orders', function (req, res) {
+app.delete('/api/orders', auth, function (req, res) {
   OrderModel.remove(function (err) {
     if (!err) {
-      console.log("removed");
       return res.send('');
     } else {
       console.log(err);
